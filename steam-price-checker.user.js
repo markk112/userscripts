4a6776d71bc59948be0fb978e54ba8a069d38d7b
cu-rve <72937506+cu-rve@users.noreply.github.com>
2021-08-09 17:02:32 +0100

    uploaded v1.0.0 of steam price checker

---
+++ steam-price-checker.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        Steam Game Price Checker
+// @version     1.0.0
+// @description A userscript that displays the lowest prices for the current viewed Steam game
+// @license     MIT
+// @author      markk112
+// @namespace   https://github.com/markk112
+// @include     https://github.com/*
+// @include     https://gist.github.com/*
+// @include     https://help.github.com/*
+// @run-at      document-idle
+// @grant       GM_addStyle
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_registerMenuCommand
+// @require     https://greasyfork.org/scripts/28721-mutations/code/mutations.js?version=952601
+// @icon        https://github.githubassets.com/pinned-octocat.svg
+// @updateURL   https://raw.githubusercontent.com/Mottie/GitHub-userscripts/master/github-collapse-markdown.user.js
+// @downloadURL https://raw.githubusercontent.com/Mottie/GitHub-userscripts/master/github-collapse-markdown.user.js
+// @supportURL  https://github.com/Mottie/GitHub-userscripts/issues
+// ==/UserScript==
