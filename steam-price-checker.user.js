baa12cbc3ac007b4a19402e903a2cf4bd7441803
cu-rve <72937506+cu-rve@users.noreply.github.com>
2021-08-09 17:07:44 +0100

    steam-price-checker header update

--- steam-price-checker.user.js
+++ steam-price-checker.user.js
@@ -5,17 +5,10 @@
 // @license     MIT
 // @author      markk112
 // @namespace   https://github.com/markk112
-// @include     https://github.com/*
-// @include     https://gist.github.com/*
-// @include     https://help.github.com/*
-// @run-at      document-idle
-// @grant       GM_addStyle
-// @grant       GM_getValue
-// @grant       GM_setValue
-// @grant       GM_registerMenuCommand
-// @require     https://greasyfork.org/scripts/28721-mutations/code/mutations.js?version=952601
-// @icon        https://github.githubassets.com/pinned-octocat.svg
-// @updateURL   https://raw.githubusercontent.com/Mottie/GitHub-userscripts/master/github-collapse-markdown.user.js
-// @downloadURL https://raw.githubusercontent.com/Mottie/GitHub-userscripts/master/github-collapse-markdown.user.js
-// @supportURL  https://github.com/Mottie/GitHub-userscripts/issues
+// @match       https://store.steampowered.com/app/*
+// @grant       GM_xmlhttpRequest
+// @icon        https://steamcommunity.com/favicon.ico
+// @updateURL   https://github.com/markk112/userscripts/raw/main/steam-price-checker.user.js
+// @downloadURL https://github.com/markk112/userscripts/raw/main/steam-price-checker.user.js
+// @supportURL  https://github.com/markk112/userscripts/issues
 // ==/UserScript==
