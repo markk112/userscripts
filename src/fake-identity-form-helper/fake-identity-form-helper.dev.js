c27c1b69e864c9bed09a10ef8b80d344fc2de02a
cu-rve <72937506+cu-rve@users.noreply.github.com>
2021-09-06 15:05:42 +0100

    created new userscript project and started creating button toggle

---
+++ src/fake-identity-form-helper/fake-identity-form-helper.dev.js
@@ -0,0 +1,57 @@
+(() => {
+    'use strict';
+
+    function injectToolkitToggle() {
+        const buttonWrapper = document.createElement('div');
+        buttonWrapper.classList.add('fifh-toggle-wrapper');
+        buttonWrapper.innerHTML = `
+            <button><i class="fas fa-user-shield"></i></button>
+        `.trim();
+        document.body.appendChild(buttonWrapper);
+    }
+
+    function injectStyles() {
+        const faLinkNode = document.createElement('link');
+        faLinkNode.setAttribute('rel', 'stylesheet');
+        faLinkNode.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css');
+        const styleNode = document.createElement('style');
+        styleNode.setAttribute('type', 'text/css');
+        styleNode.innerHTML = `
+            .fifh-toggle-wrapper {
+                position: fixed;
+                top: 200px;
+                z-index: 1000;
+            }
+            .fifh-toggle-wrapper button {
+                height: 60px;
+                width: 60px;
+                cursor: pointer;
+                line-height: 1;
+                text-decoration: none;
+                background-color: #FFFFFF;
+                border-top: 1px solid #E0E0E0;
+                border-bottom: 1px solid #E0E0E0;
+                border-right: 1px solid #E0E0E0;
+                border-left: none;
+                border-radius: 0 6px 6px 0;
+            }
+            .fifh-toggle-wrapper button i {
+                color: #030303;
+                font-size: 30px;
+            }
+            .fifh-toggle-wrapper button:focus {
+                outline: none;
+            }
+        `.trim();
+        document.head.appendChild(faLinkNode);
+        document.head.appendChild(styleNode);
+    }
+
+    function init() {
+        injectStyles();
+        injectToolkitToggle();
+    }
+
+    init();
+
+})();
